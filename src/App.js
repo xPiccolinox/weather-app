09987190f8e34d58fd268476984a12a3c80bf43d
Jakub Sobór <kupkeu@gmail.com>
2022-08-28 14:12:42 +0200

    Feat: Sidebar menu prototype

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Menu from './Menu/Menu';
 
 function App() {
+
+
   return (
     <BrowserRouter>
       <div className="App">
+        <img className="weatherBackground" src="./images/weather.png"  alt="weather.png" />
         <div className="searchBar">
 
         </div>
-        <div className="menu">
-
-        </div>
+        <Menu />
         <div className="weather">
 
         </div>
