02598b69c0a2a03a21fb34cce4eb28d5f66d40a4
Jakub Sobór <kupkeu@gmail.com>
2022-09-20 21:36:16 +0200

    Feat: WeatherTileMain finished prototype layout

--- src/Components/WeatherTileMain/WeatherTileMain.js
+++ src/Components/WeatherTileMain/WeatherTileMain.js
@@ -1,25 +1,35 @@
 import './WeatherTileMain.css'
 
 const WeatherTileMain = (props) => {
-  const className=`weatherTileMain tileWeather_${props.tileWeather}`
+  const className = `weatherTileMain tileWeather_${props.tileWeather}`
+  const img = './icons/weather_1.png'
 
   return(
     <div className={className}>
       <div className="weatherInfo"> 
         <div className="weatherInfo_date">
-          <h1>One</h1>
-          <h1>Two</h1>
-          <h1>Three</h1>
+          <div className="day">Day</div>
+          <div className="dayName">Day Name</div>
+          <div className="time">Hour</div>
         </div>
         <div className="weatherInfo_img">
-
+          <img src={img} alt="weatherIcon"/>
         </div>
         <div className="weatherInfo_temp">
-
+          <div className="temp">Temp</div>
+          <div className="tempName">Temp Name</div>
+          <div className="maxTemp">Max Temp</div>
+          <div className="minTemp">Min Temp</div>
         </div>
       </div>
       <div className="weatherHourlyInfo">
-
+        <div></div>
+        <div className="separation"></div>
+        <div></div>
+        <div className="separation"></div>
+        <div></div>
+        <div className="separation"></div>
+        <div></div>
       </div>
     </div>
   )
