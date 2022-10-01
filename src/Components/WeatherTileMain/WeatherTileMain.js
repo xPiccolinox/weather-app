3f56a5211df63d518535236a6f5ba8edaab52235
Jakub Sobór <kupkeu@gmail.com>
2022-10-01 23:10:29 +0200

    Feat: weatherTileMain weatherHourlyInfoDiv css

--- src/Components/WeatherTileMain/WeatherTileMain.js
+++ src/Components/WeatherTileMain/WeatherTileMain.js
@@ -2,7 +2,10 @@ import './WeatherTileMain.css'
 
 const WeatherTileMain = (props) => {
   const className = `weatherTileMain tileWeather_${props.tileWeather}`
-  const img = './icons/weather_1.png'
+  const img1 = './icons/weather_1.png'
+  const img2 = './icons/weather_2.png'
+  const img3 = './icons/weather_3.png'
+  const img4 = './icons/weather_4.png'
 
   return(
     <div className={className}>
@@ -11,11 +14,11 @@ const WeatherTileMain = (props) => {
           <div className="day">Today</div>
           <div className="date">18 September 2022</div>
           <div className="time">12:00
-            <sup>AM</sup>
+            <div className="period">AM</div>
           </div>
         </div>
         <div className="weatherInfo_img">
-          <img src={img} alt="weatherIcon"/>
+          <img src={img1} alt="weatherIcon"/>
         </div>
         <div className="weatherInfo_temp">
           <div className="temp">
@@ -44,28 +47,55 @@ const WeatherTileMain = (props) => {
       </div>
       <div className="weatherHourlyInfo">
         <div className="weatherHourlyTile">
-          <img src={img} alt="weatherHourlyInfoIcon"/>
+          <img src={img1} alt="weatherHourlyInfoIcon"/>
           <div className="tempDiv">
-              29
+              31
               <div className="tempDegree">&#176;</div>
               <div className="tempCelcius">c</div>
             </div>
             <div className="timeDiv">
               3:00
-              <sup>PM</sup>
+              <div className="period">PM</div>
             </div>
         </div>
         <div className="weatherHourlySeparation"/>
         <div className="weatherHourlyTile">
-
+          <img src={img2} alt="weatherHourlyInfoIcon"/>
+          <div className="tempDiv">
+              28
+              <div className="tempDegree">&#176;</div>
+              <div className="tempCelcius">c</div>
+            </div>
+            <div className="timeDiv">
+              6:00
+              <div className="period">PM</div>
+            </div>
         </div>
         <div className="weatherHourlySeparation"/>
         <div className="weatherHourlyTile">
-
+          <img src={img3} alt="weatherHourlyInfoIcon"/>
+          <div className="tempDiv">
+              26
+              <div className="tempDegree">&#176;</div>
+              <div className="tempCelcius">c</div>
+            </div>
+            <div className="timeDiv">
+              9:00
+              <div className="period">PM</div>
+            </div>
         </div>
         <div className="weatherHourlySeparation"/>
         <div className="weatherHourlyTile">
-
+          <img src={img4} alt="weatherHourlyInfoIcon"/>
+          <div className="tempDiv">
+              24
+              <div className="tempDegree">&#176;</div>
+              <div className="tempCelcius">c</div>
+            </div>
+            <div className="timeDiv">
+              12:00
+              <div className="period">PM</div>
+            </div>
         </div>
       </div>
     </div>
